271cc2fdb110cae2109be2813533e7ea55fb12ae
root <root@ECE-Club-Ubuntu.(none)>
2014-07-12 22:22:33 -0400

    changed the loop

--- wp-content/themes/ececlub/front-page.php
+++ wp-content/themes/ececlub/front-page.php
@@ -3,8 +3,14 @@
 	<?php while ( have_posts() ) : the_post(); ?>
 		<section class="col-8 col-md-8">
 			<div class="post">
+				<div id="colorstrip"></div>
+				<div class="post-content">
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
+				</div>
+				<div class="container timestamp">
+					Submitted by <?php the_author ?> on <?php the_date('l, F j, Y'); ?>
+				</div>
 			</div>
 		</section>
 
