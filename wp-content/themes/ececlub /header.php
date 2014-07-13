32b6908a58bf35af95799233518e77b020d39b20
mwx1993 <mawenxin52@hotmail.com>
2014-07-13 15:58:24 -0400

    trying to deal with conflict

--- wp-content/themes/ececlub /header.php
+++ wp-content/themes/ececlub /header.php
@@ -16,7 +16,6 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css" />
-	
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 
 	<?php wp_head(); ?>
