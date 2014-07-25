b8c3dea3bcdb32061df8a4d1e374303f443cb922
root <root@ECE-Club-Ubuntu.(none)>
2014-07-24 22:53:35 -0400

    changed constitution

--- wp-content/themes/ececlub/page-constitution.php
+++ wp-content/themes/ececlub/page-constitution.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
-	hello, hows going
 
 <section id="constitution-container">
 	<div id="frame-const">
+		<h4>Electrical Engineering Undergraduate Club Constitution</h4>
 		<iframe src="<?php echo get_template_directory_uri(); ?>/required-docs/Electrical Club Constitution 1T5.pdf"></iframe>	
+		<h4>Computer Engineering Undergraduate Club Constitution</h4>
+		<iframe src="<?php echo get_template_directory_uri(); ?>/required-docs/Computer Club Constitution 1T5.pdf"></iframe>	
 	</div>
 </section>
 
