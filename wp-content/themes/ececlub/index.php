b610dd15d98d5168dd684a20464e1a0fa73a4499
TomZhangUT <tomzhang94@gmail.com>
2014-09-18 21:59:00 -0400

    fixed autodownload problem for pdfs
    Use pdf-frame for showing pdfs

--- wp-content/themes/ececlub/index.php
+++ wp-content/themes/ececlub/index.php
@@ -1,13 +1,19 @@
 <?php get_header(); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-	<section class="blog-container">
-		<div class="blog-text-container">
-			
-			<h1><?php the_title(); ?></h1>
-			<?php the_content(); ?>
+<div class="page-container container-fluid">
+		<div class="row-fluid ">
+			<div class="col-md-7 col-md-offset-3">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<section class="blog-container">
+						<div class="blog-text-container">
+							<h1><?php the_title(); ?></h1>
+							<hr class="horizontal-rule">
+							<?php the_content(); ?>
+						</div>
+					</section>
+				
+				<?php endwhile; ?>
+			</div>
 		</div>
-			<?php comments_template( $file, $separate_comments ); ?>
-	</section>
-
-<?php endwhile; ?>
+	</div>
+</div>
 <?php get_footer(); ?>
